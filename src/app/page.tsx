18a8df938e0ae09ad2fc505bbed5cbea7e845677
2fill <ichaeu@naver.com>
2024-10-10 17:11:21 +0900

    Chore: 코드 설명 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+/* Next.js 라우팅 */
 import Link from "next/link";
 
 interface Category{
@@ -6,19 +7,25 @@ interface Category{
     strCategoryThumb: string;
 }
 
-const fetchCategory = async (): Promise <Category[]> => {
+/* 카테고리 데이터 가져오는 비동기 함수, Category 배열 반환 */
+const fetchCategory = async(): Promise <Category[]> => {
+    /* API 요청 */
     const response = await fetch('https://www.themealdb.com/api/json/v1/1/categories.php');
+    /* API 응답 json 형식으로 변환 */
     const data = await response.json();
+    /* 'categories' 속성 반환 */
     return data.categories;
 };
 
 const Home = async() => {
+    /* fetchCategory 함수로 데이터 가져옴 */
     const categories: Category[] = await fetchCategory();
     
     return(
         <div className = "page">
             <h4 className = "category-title">Category</h4>
             {categories.length > 0 ? (
+                // categories 배열을 순회
                 categories.map((category) => (
                 <Link href = {`/menu?name=${encodeURIComponent(category.strCategory)}`} key = {category.idCategory}>
                     <div className = "category">
